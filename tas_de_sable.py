4074d4fbacbfe7667a4644d121b7b0620e271e04
Eliott MOBRE <eliott.mobre@gmail.com>
2022-02-11 11:31:09 +0100

    ajoute le code

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -16,3 +16,36 @@ bonsoir
 
 
 #PARTIE CODE 
+
+#création de la grille et répartition du sable
+width, height, nbrInit  = 5, 5, 32
+grille =  [[0 for i in range(width)] for j in range(height)]
+grille[width//2][height//2] = nbrInit
+print(grille)
+
+def SandMove():
+    
+    global grille
+    newGrille = [[0 for i in range(width)] for j in range(height)]
+
+    for x in range(width):
+        for y in range(height):
+            num = grille[x][y]
+            if num < 4:
+                newGrille[x][y] = grille[x][y]
+
+    for x in range(width):
+        for y in range(height):
+            num = grille[x][y]
+            if num >= 4:
+                newGrille[x][y] = grille[x][y] -4
+                newGrille[x+1][y] +=1
+                newGrille[x-1][y] +=1
+                newGrille[x][y+1] +=1
+                newGrille[x][y-1] +=1
+    grille = newGrille
+    print(grille)
+
+for u in range(12):
+    SandMove()
+    sleep(2)
