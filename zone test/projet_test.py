d0138e45324b53c305fb3163a6cd4647931057bf
Jean-Alexandre SUZANNE <jean-alexandre.suzanne@ens.uvsq.fr>
2022-02-22 09:49:51 +0100

    ras

--- zone test/projet_test.py
+++ zone test/projet_test.py
@@ -22,8 +22,7 @@ from tkinter import font
 from turtle import bgcolor
 from tkinter import messagebox as box
 from tkinter import Entry 
-import tkinter as tk
-from time import sleep
+from tkinter import Label
 
 ######################
 #Fenêtres Affichages
@@ -46,7 +45,7 @@ def start():
         #T = int(entry_temp.get())
     go = True
     T = 0
-    commande.destroy()
+    #commande.destroy()
 
 
 def temporaire():
@@ -63,25 +62,9 @@ def stop():
 
 def reload():
     global T
-    racine.destroy()
+    #racine.destroy()
     T = 1
     
-while T == 1 :
-    commande = tk.Tk()
-    canvas_temp = tk.Canvas(commande)
-    entry = Entry(commande, text="Tapez la taille du carré")
-    #entry_temp = Entry(commande)
-    bouton_start = tk.Button(commande, text="Commencer la simulation", command=start)
-    bouton_stop = tk.Button(commande, text="Stop", command=stop)
-    #bouton_temporaire = tk.Button(commande, text="temporaire", command=temporaire)
-    entry.grid(row=1)
-    #entry_temp.grid(row=2)
-    bouton_start.grid(row = 3)
-    bouton_stop.grid(row=4)
-    #bouton_temporaire.grid(row=5)
-
-    commande.mainloop()
-
 
 ######################
 #PARTIE CODE 
@@ -144,32 +127,54 @@ if go == True:
     grille =  [[0 for i in range(long)] for j in range(long)]
     grille[long//2][long//2] = 16
 
-    racine = tk.Tk()
-    racine.title("Tas de sable")
-    bouton_reload = tk.Button(racine, text="Redémarrer la simulation", command=reload)
-    canvas = tk.Canvas(racine, height=450, width=450)
-    racine.eval('tk::PlaceWindow %s center' % racine.winfo_toplevel())
-    bouton_reload.grid(row = 1)
+    #racine = tk.Tk()
+    #commande.title("Tas de sable")
+    #bouton_reload = tk.Button(racine, text="Redémarrer la simulation", command=reload)
+    #canvas = tk.Canvas(racine, height=450, width=450)
+    #commande.eval('tk::PlaceWindow %s center' % commande.winfo_toplevel())
+    #bouton_reload.grid(row = 1)
 
+if T == 1:
+    commande = tk.Tk()
+    canvas = tk.Canvas(commande, height=500, width=500)
+    entry = Entry(commande, text="Tapez la taille du carré")
+    #entry_temp = Entry(commande)
+    bouton_start = tk.Button(commande, text="Commencer la simulation", command=start)
+    bouton_stop = tk.Button(commande, text="Stop", command=stop)
+    #bouton_temporaire = tk.Button(commande, text="temporaire", command=temporaire)
+    canvas.grid(column=2, row=2)
+    carree = Label(commande, text="Choisir la taille du carré :")
+    entry.grid(column=1,row=0)
+    carree.grid(column=0, row=0)
+    #entry_temp.grid(row=2)
+    bouton_start.grid(column=0,row = 1)
+    bouton_stop.grid(column=1,row=1)
+    #bouton_temporaire.grid(row=5)
+
+if go == True:
     show = []
     for x in range(long):
         columnShow = []
         for y in range(long):
-            columnShow.append(canvas.create_rectangle(450/long*x,450/long*y,50+450/long*x,50+450/long*y,fill=FindColor(x,y,grille),outline="black") )
+            rectangle = canvas.create_rectangle(450/long*x,450/long*y,50+450/long*x,50+450/long*y,fill=FindColor(x,y,grille),outline="black")
+            columnShow.append(rectangle)
         show.append(columnShow)
-
-
 
     for o in range(10) :
         bordureFill(grille,long,'#')
         for x in range(long):
             for y in range(long):
+                canvas.grid(column=2)  
                 canvas.itemconfig(show[x][y], fill=FindColor(x,y,grille))
-                canvas.grid()  
+                
 
-    sleep(1)  
-    bordureFill(grille,long,0)
-    sandMove(long)
 
-    racine.mainloop()
+
+commande.mainloop()
+
+sleep(1)  
+bordureFill(grille,long,0)
+sandMove(long)
+
+    #racine.mainloop()
 
